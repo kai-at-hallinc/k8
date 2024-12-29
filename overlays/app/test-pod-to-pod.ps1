0ecfff95046bc4a5903bc6af9887060061d90267
kai-at-hallinc <kai@hallinc.onmicrosoft.com>
2024-12-29 21:04:40 +0200

    added bash test for pod2pod.

--- overlays/app/test-pod-to-pod.ps1
+++ overlays/app/test-pod-to-pod.ps1
@@ -8,7 +8,8 @@ function Test-HTMLContent {
 }
 
 # allocation to optimization
-$res1 = (kubectl exec allocation-be-69657c6b64-xm5pt -n test --  curl -s optimization-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("allocation-be")) | .metadata.name')
+$res1 = (kubectl exec $cont -n test --  curl -s optimization-svc.test.svc.cluster.local:80)
 $string =  "optimization"
 if (-not (Test-HTMLContent -htmlContent $res1 -expectedString $string)){
     Write-Error "Error: allocation-optimization returned unexpected HTML content"
@@ -16,7 +17,8 @@ if (-not (Test-HTMLContent -htmlContent $res1 -expectedString $string)){
 }
 
 # allocation to rabbitmq
-$res2 = (kubectl exec allocation-be-69657c6b64-xm5pt -n test --  curl -s rabbitmq-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("allocation-be")) | .metadata.name')
+$res2 = (kubectl exec $cont -n test --  curl -s rabbitmq-svc.test.svc.cluster.local:80)
 $string =  "rabbitmq"
 if (-not (Test-HTMLContent -htmlContent $res2 -expectedString $string)){
     Write-Error "Error: allocation-rabbitmq returned unexpected HTML content"
@@ -24,7 +26,8 @@ if (-not (Test-HTMLContent -htmlContent $res2 -expectedString $string)){
 }
 
 # allocation to celery
-$res3 = (kubectl exec allocation-be-69657c6b64-xm5pt -n test --  curl -s celery-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("allocation-be")) | .metadata.name')
+$res3 = (kubectl exec $cont -n test --  curl -s celery-svc.test.svc.cluster.local:80)
 $string =  "celery"
 if (-not (Test-HTMLContent -htmlContent $res3 -expectedString $string)){
     Write-Error "Error: allocation-celery returned unexpected HTML content"
@@ -32,35 +35,40 @@ if (-not (Test-HTMLContent -htmlContent $res3 -expectedString $string)){
 }
 
 # optimization to rabbitmq
-$res4 = (kubectl exec optimization-86bc7cc685-76pgc -n test --  curl -s rabbitmq-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("optimization")) | .metadata.name')
+$res4 = (kubectl exec $cont -n test --  curl -s rabbitmq-svc.test.svc.cluster.local:80)
 $string =  "rabbitmq"
 if (-not (Test-HTMLContent -htmlContent $res4 -expectedString $string)){
     Write-Error "Error: optimization-rabbitmq returned unexpected HTML content"
     return
 }
 # optimization to celery
-$res5 = (kubectl exec optimization-86bc7cc685-76pgc -n test --  curl -s celery-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("optimization")) | .metadata.name')
+$res5 = (kubectl exec $cont -n test --  curl -s celery-svc.test.svc.cluster.local:80)
 $string =  "celery"
 if (-not (Test-HTMLContent -htmlContent $res5 -expectedString $string)){
     Write-Error "Error: optimization-celery returned unexpected HTML content"
     return
 }
 # rabbitmq to celery
-$res6 = (kubectl exec rabbitmq-7d6bcdb78b-hbbgh -n test --  curl -s celery-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("rabbitmq")) | .metadata.name')
+$res6 = (kubectl exec $cont -n test --  curl -s celery-svc.test.svc.cluster.local:80)
 $string =  "celery"
 if (-not (Test-HTMLContent -htmlContent $res6 -expectedString $string)){
     Write-Error "Error: rabbitmq-celery returned unexpected HTML content"
     return
 }
 # allocation frontend to backend
-$res7 = (kubectl exec allocation-fe-56c4fc98b-p4bxl -n test --  curl -s allocation-be-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("allocation-fe")) | .metadata.name')
+$res7 = (kubectl exec $cont -n test --  curl -s allocation-be-svc.test.svc.cluster.local:80)
 $string =  "Backend"
 if (-not (Test-HTMLContent -htmlContent $res7 -expectedString $string)){
     Write-Error "Error: allocation fe-be returned unexpected HTML content"
     return
 }
 # timetable frontend to backend
-$res8 = (kubectl exec timetable-fe-8cb49558f-bgnl8 -n test --  curl -s timetable-be-svc.test.svc.cluster.local:80)
+$cont = (kubectl get pods -n test -o json | jq -r '.items[] | select(.metadata.name | contains("timetable-fe")) | .metadata.name')
+$res8 = (kubectl exec $cont -n test --  curl -s timetable-be-svc.test.svc.cluster.local:80)
 $string =  "Backend"
 if (-not (Test-HTMLContent -htmlContent $res8 -expectedString $string)){
     Write-Error "Error: timetable fe-be returned unexpected HTML content"
--- overlays/app/test-pod-to-pod.sh
+++ overlays/app/test-pod-to-pod.sh
@@ -11,8 +11,11 @@ function Test_HTMLContent {
     fi
 }
 
+# Get pod names containing 'allocation' in the 'test' namespace
+allocation_pods=$(kubectl get pods -n test | grep 'allocation' | awk '{print $1}')
+
 # allocation to optimization
-res1=$(kubectl exec allocation-be-69657c6b64-xm5pt -n test -- curl -s optimization-svc.test.svc.cluster.local:80)
+res1=$(kubectl exec $(echo $allocation_pods | awk '{print $1}') -n test -- curl -s optimization-svc.test.svc.cluster.local:80)
 string="optimization"
 if ! Test_HTMLContent "$res1" "$string"; then
     echo "Error: allocation-optimization returned unexpected HTML content" >&2
@@ -20,7 +23,7 @@ if ! Test_HTMLContent "$res1" "$string"; then
 fi
 
 # allocation to rabbitmq
-res2=$(kubectl exec allocation-be-69657c6b64-xm5pt -n test -- curl -s rabbitmq-svc.test.svc.cluster.local:80)
+res2=$(kubectl exec $(echo $allocation_pods | awk '{print $1}') -n test -- curl -s rabbitmq-svc.test.svc.cluster.local:80)
 string="rabbitmq"
 if ! Test_HTMLContent "$res2" "$string"; then
     echo "Error: allocation-rabbitmq returned unexpected HTML content" >&2
@@ -28,15 +31,18 @@ if ! Test_HTMLContent "$res2" "$string"; then
 fi
 
 # allocation to celery
-res3=$(kubectl exec allocation-be-69657c6b64-xm5pt -n test -- curl -s celery-svc.test.svc.cluster.local:80)
+res3=$(kubectl exec $(echo $allocation_pods | awk '{print $1}') -n test -- curl -s celery-svc.test.svc.cluster.local:80)
 string="celery"
 if ! Test_HTMLContent "$res3" "$string"; then
     echo "Error: allocation-celery returned unexpected HTML content" >&2
     exit 1
 fi
 
+# Get pod names containing 'optimization' in the 'test' namespace
+optimization_pods=$(kubectl get pods -n test | grep 'optimization' | awk '{print $1}')
+
 # optimization to rabbitmq
-res4=$(kubectl exec optimization-86bc7cc685-76pgc -n test -- curl -s rabbitmq-svc.test.svc.cluster.local:80)
+res4=$(kubectl exec $(echo $optimization_pods | awk '{print $1}') -n test -- curl -s rabbitmq-svc.test.svc.cluster.local:80)
 string="rabbitmq"
 if ! Test_HTMLContent "$res4" "$string"; then
     echo "Error: optimization-rabbitmq returned unexpected HTML content" >&2
@@ -44,31 +50,38 @@ if ! Test_HTMLContent "$res4" "$string"; then
 fi
 
 # optimization to celery
-res5=$(kubectl exec optimization-86bc7cc685-76pgc -n test -- curl -s celery-svc.test.svc.cluster.local:80)
+res5=$(kubectl exec $(echo $optimization_pods | awk '{print $1}') -n test -- curl -s celery-svc.test.svc.cluster.local:80)
 string="celery"
 if ! Test_HTMLContent "$res5" "$string"; then
     echo "Error: optimization-celery returned unexpected HTML content" >&2
     exit 1
 fi
 
+# Get pod names containing 'rabbitmq' in the 'test' namespace
+rabbitmq_pods=$(kubectl get pods -n test | grep 'rabbitmq' | awk '{print $1}')
+
 # rabbitmq to celery
-res6=$(kubectl exec rabbitmq-7d6bcdb78b-hbbgh -n test -- curl -s celery-svc.test.svc.cluster.local:80)
+res6=$(kubectl exec $(echo $rabbitmq_pods | awk '{print $1}') -n test -- curl -s celery-svc.test.svc.cluster.local:80)
 string="celery"
 if ! Test_HTMLContent "$res6" "$string"; then
     echo "Error: rabbitmq-celery returned unexpected HTML content" >&2
     exit 1
 fi
 
+# Get pod names containing 'allocation-fe' in the 'test' namespace
+allocation_fe_pods=$(kubectl get pods -n test | grep 'allocation-fe' | awk '{print $1}')
 # allocation frontend to backend
-res7=$(kubectl exec allocation-fe-56c4fc98b-p4bxl -n test -- curl -s allocation-be-svc.test.svc.cluster.local:80)
+res7=$(kubectl exec $(echo $allocation_fe_pods | awk '{print $1}') -n test -- curl -s allocation-be-svc.test.svc.cluster.local:80)
 string="Backend"
 if ! Test_HTMLContent "$res7" "$string"; then
     echo "Error: allocation fe-be returned unexpected HTML content" >&2
     exit 1
 fi
 
+# Get pod names containing 'timetable-fe' in the 'test' namespace
+timetable_fe_pods=$(kubectl get pods -n test | grep 'timetable-fe' | awk '{print $1}')
 # timetable frontend to backend
-res8=$(kubectl exec timetable-fe-8cb49558f-bgnl8 -n test -- curl -s timetable-be-svc.test.svc.cluster.local:80)
+res8=$(kubectl exec $(echo $timetable_fe_pods | awk '{print $1}') -n test -- curl -s timetable-be-svc.test.svc.cluster.local:80)
 string="Backend"
 if ! Test_HTMLContent "$res8" "$string"; then
     echo "Error: timetable fe-be returned unexpected HTML content" >&2
