80122d12e2b739a2a5715051475a818a901f2486
kai-at-hallinc <kai@hallinc.onmicrosoft.com>
2025-01-01 18:55:59 +0200

    formatting fix.

--- overlays/app/main.tf
+++ overlays/app/main.tf
@@ -98,7 +98,7 @@ resource "helm_release" "nginx-ingress" {
   ## This is where the public static IP address must be located:-> managed rg
   set {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-resource-group"
-    value = data.azurerm_resource_group.aks_rg.name 
+    value = data.azurerm_resource_group.aks_rg.name
   }
 
   set {
